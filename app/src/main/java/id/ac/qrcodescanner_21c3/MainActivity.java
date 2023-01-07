6bd96533cb8aae4c34246feecaaaf3a8640ccb83
rifkyyasa <rifkyyasa97@gmail.com>
2023-01-07 22:23:50 +0700

    5 fungsi

--- app/src/main/java/id/ac/qrcodescanner_21c3/MainActivity.java
+++ app/src/main/java/id/ac/qrcodescanner_21c3/MainActivity.java
@@ -53,6 +53,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             } else if (Patterns.WEB_URL.matcher(result.getContents()).matches()) {
                 Intent visitUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(result.getContents()));
                 startActivity(visitUrl);
+
+            } else if (Patterns.EMAIL_ADDRESS.matcher(result.getContents()).matches()) {
+                Intent intent = new Intent(Intent.ACTION_SEND, Uri.parse(result.getContents()));
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"raihanadzuhri1@gmail.com"});
+                intent.putExtra(Intent.EXTRA_CC, new String[]{"raihancahbekasi1@gmail.com"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Fungsi Email");
+                intent.putExtra(Intent.EXTRA_TEXT, "Raihan Adzuhri-TI.21.C.3-312110374");
+                try {
+                    startActivity(Intent.createChooser(intent, "Ingin Mengirim Email ?"));
+                } catch (android.content.ActivityNotFoundException ex) {
+                }
+            }else if (Patterns.WEB_URL.matcher(result.getContents()).matches()){
+                String geoUri=result.getContents();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(geoUri));
+                //Set Package
+                intent.setPackage("com.google.android.apps.maps");
+
+                //Set Flag
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                startActivity(intent);
             }
             else if (Patterns.PHONE.matcher(result.getContents()).matches()) {
                 String telp = String.valueOf(result.getContents());
@@ -64,18 +86,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 } catch (android.content.ActivityNotFoundException ex) {
                     Toast.makeText(MainActivity.this, "no phone apk installed.", Toast.LENGTH_LONG);
                 }
-            }else if (Patterns.EMAIL_ADDRESS.matcher(result.getContents()).matches()) {
-                Intent intent = new Intent(Intent.ACTION_SEND, Uri.parse(result.getContents()));
-                intent.setType("text/plain");
-                intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"rifkyyasa97@gmail.com"});
-                intent.putExtra(Intent.EXTRA_CC, new String[] {"guntoroagun@gmail.com"});
-                intent.putExtra(Intent.EXTRA_SUBJECT, "Fungsi Email");
-                intent.putExtra(Intent.EXTRA_TEXT, "M.Rifky Yasa Putra-TI.21.C.3-312110211");
-                try {
-                    startActivity(Intent.createChooser(intent, "Ingin Mengirim Email ?"));
-                } catch (android.content.ActivityNotFoundException ex) {
-                    //do something else
-                }}
+            }
             else {
                 try {
                     //konversi datanya ke json
@@ -89,22 +100,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Toast.makeText(this, result.getContents(),
                             Toast.LENGTH_LONG).show();
                 }
-            } }{
-            try {
-                String geoUri=result.getContents();
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(geoUri));
-                //Set Package
-                intent.setPackage("com.google.android.apps.maps");
+            } }
 
-                //Set Flag
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-                startActivity(intent);
-            }finally {
-
-            }
-
-        }  {
+        else{
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
@@ -113,8 +111,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //inisialisasi qrcode scanning
         qrScan.initiateScan();
 
-    }
-}
+
+}}
 
 
 
